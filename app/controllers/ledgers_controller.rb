9770f16f18dd858f32978e005e83e997d93a6fa7
Pawan Sunuwar <pawanman56@gmail.com>
2016-12-21 15:09:16 +0000

    Final commit

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -1,6 +1,10 @@
 class LedgersController < ApplicationController
     def index
-        @ledgers = Ledger.all
+        @ledgers = Ledger.order("created_at DESC")
+        @blnc = @ledgers.balance
+        @dbt = @ledgers.debt
+        @amt = @ledgers.amount
+        
     end
     
     def create
@@ -15,12 +19,19 @@ class LedgersController < ApplicationController
     def update
         @ledger = Ledger.find(params[:id])
         if @ledger.update(ledger_params)
-            flash[:success] = "This entry has been updated!"
+            #flash[:success] = "This entry has been updated!"
             redirect_to root_path
         else
-            flash[:alert] = "Woops! Looks like there is an error."
+            #flash[:alert] = "Woops! Looks like there is an error."
             redirect_to edit_ledger_path(params[:id])
         end
+    end
+    
+    def destroy
+        @ledger = Ledger.find(params[:id])
+        @ledger.destroy
+        flash[:success] = "This idea has been deleted!"
+        redirect_to root_path
     end
 
     private
--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,2 +1,16 @@
 class Ledger < ActiveRecord::Base
+    
+    validates :title, :amount, :date, presence: true
+    
+    def self.amount
+        sum(:amount)
+    end
+    
+    def self.balance
+        where("amount > ?",0).sum(:amount)
+    end
+    
+    def self.debt
+        where("amount < ?",0).sum(:amount)
+    end
 end
